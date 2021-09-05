b634e2dbfc9cc86e9d8430c1dd88095871647383
trim0917 <trim0917@gmail.com>
2021-09-06 01:01:51 +0900

    로그 수정

--- KosherUtils/Log/Log.cs
+++ KosherUtils/Log/Log.cs
@@ -1,4 +1,9 @@
-﻿namespace KosherUtils.Log
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace KosherUtils.Log
 {
     public class Log
     {
@@ -15,5 +20,62 @@
         {
             logger.Error(logMessage);
         }
+        public static void Error(Exception ex)
+        {
+            var st = new StackTrace(ex, true);
+            var frames = st.GetFrames().Select(f =>
+            new
+            {
+                FileName = f.GetFileName(),
+                LineNumber = f.GetFileLineNumber(),
+                ColumnNumber = f.GetFileColumnNumber(),
+                Method = f.GetMethod(),
+                Class = f.GetMethod().DeclaringType,
+            });
+            foreach (var frame in frames)
+            {
+                if (string.IsNullOrEmpty(frame.FileName))
+                    continue;
+                logger.Error($"[ {frame.FileName} : {frame.LineNumber} ] [ {ex.Message} ]");
+                logger.Error(frame.FileName, frame.LineNumber);
+            }
+        }
+        public static void Warning(Exception ex)
+        {
+            var st = new StackTrace(ex, true);
+            var frames = st.GetFrames().Select(f =>
+            new
+            {
+                FileName = f.GetFileName(),
+                LineNumber = f.GetFileLineNumber(),
+                ColumnNumber = f.GetFileColumnNumber(),
+                Method = f.GetMethod(),
+                Class = f.GetMethod().DeclaringType,
+            });
+            foreach (var frame in frames)
+            {
+                if (string.IsNullOrEmpty(frame.FileName))
+                    continue;
+
+                logger.Warn($"[ {frame.FileName} : {frame.LineNumber} ] [ {ex.Message} ]");
+            }
+        }
+        public static void Debug(string message, [CallerLineNumber] int callerLine = 0, [CallerFilePath] string className = "")
+        {
+            if (string.IsNullOrWhiteSpace(className))
+            {
+                throw new ArgumentException("message", nameof(className));
+            }
+            logger.Debug(message, callerLine);
+        }
+        public static void DebugWriteFile(string message, [CallerLineNumber] int callerLine = 0, [CallerFilePath] string className = "")
+        {
+            if (string.IsNullOrWhiteSpace(className))
+            {
+                throw new ArgumentException("message", nameof(className));
+            }
+            logger.Error(message, callerLine);
+        }
+
     }
 }
